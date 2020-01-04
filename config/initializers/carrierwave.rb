b2117b0340ffc9b5bd6c6a1002540f2dbe85a257
hoshi4444 <hoshi44444444@gmail.com>
2020-01-04 14:52:01 +0900

    edit-uploader

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,3 +9,16 @@ module CarrierWave
     end
   end
 end
+
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+    region: 'ap-northeast-1'
+  }
+
+  config.fog_directory  = 'tobecontinued'
+  config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/tobecontinued"
+  config.cache_storage = :fog
+end
